e55312a8724f6357e2a601c3e0bd303286091e3c
Gonzalo melo <gonzalomelov@gmail.com>
2015-12-09 17:36:03 -0300

    Add hardcoded token

--- library/src/main/java/io/teamkona/konatools/network/AuthRequestInterceptor.java
+++ library/src/main/java/io/teamkona/konatools/network/AuthRequestInterceptor.java
@@ -23,10 +23,16 @@ public class AuthRequestInterceptor implements Interceptor {
   @Override public Response intercept(Chain chain) throws IOException {
     Request.Builder newRequestbuilder = chain.request().newBuilder();
 
-    if (sessionManager.isSessionAlive()) {
-      newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
-          String.format("%s %s", HEADER_AUTHORIZATION_BEARER, sessionManager.getSession().getAuthToken()));
-    }
+    //if (sessionManager.isSessionAlive()) {
+    //  newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
+    //      String.format("%s %s", HEADER_AUTHORIZATION_BEARER, sessionManager.getSession().getAuthToken()));
+    //}
+
+    // FIXME Remove this
+    String authToken ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NjY3YjliMGJjNGI1ZDQ3MDI2MTcyYWIiLCJfY3JlYXRlZEF0IjoiMjAxNS0xMi0wOVQwNToxODo0MC41MTlaIiwiX3VwZGF0ZWRBdCI6IjIwMTUtMTItMDlUMDU6MTg6NDAuNTE5WiIsImVtYWlsIjoiaGVybmFuQHRlYW1rb25hLmlvIiwiX192IjowLCJmcmllbmRzIjpbXSwiZmF2b3VyaXRlUmVjb21tZW5kZWQiOltdLCJmYXZvdXJpdGVFdmVudHMiOltdLCJpbnRlcmVzdHMiOltdLCJjb3VudHJpZXNWaXNpdGVkIjpbXSwiY291bnRyaWVzVG9WaXNpdCI6W10sImRlc2NyaXB0aW9uIjpbXSwiY3VycmVudFBvc2l0aW9uIjpbXSwicHJvZmlsZVBob3RvcyI6W119.PNlxXUXcH09Jzhox-tG73xVDIExOZeHV2zvU0oyYKvA";
+    newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
+        String.format("%s %s", HEADER_AUTHORIZATION_BEARER, authToken));
+
 
     Request newRequest = newRequestbuilder.build();
 
