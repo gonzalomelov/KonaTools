c1428759b2fff29db3a5cdb250c1ad2365b84d32
Gonzalo Melo <gonzalo@teamkona.io>
2016-01-05 12:58:38 -0300

    Speed up and some date methods

--- library/src/main/java/io/teamkona/konatools/date/DateHelper.java
+++ library/src/main/java/io/teamkona/konatools/date/DateHelper.java
@@ -1,46 +1,61 @@
 package io.teamkona.konatools.date;
 
 import java.util.Date;
+import java.util.Locale;
 import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.LocalTime;
 import org.threeten.bp.ZoneId;
+import org.threeten.bp.format.DateTimeFormatter;
 
 /**
  * Created by gonzalomelo on 12/28/15.
  */
 public class DateHelper {
 
+  // TODO Review
+  // http://stackoverflow.com/questions/19431234/converting-between-java-time-localdatetime-and-java-util-date
+
   public final static String DATE_TIME_WITH_SECONDS = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
   public static LocalDateTime date2LocalDateTime(Date value) {
-    Date ts = value;
-    Instant instant = Instant.ofEpochMilli(ts.getTime());
-    LocalDateTime res = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-    return res;
+    Instant instant = Instant.ofEpochMilli(value.getTime());
+    return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
   }
 
   public static LocalDate date2LocalDate(Date value) {
-    Date date = value;
-    Instant instant = Instant.ofEpochMilli(date.getTime());
-    LocalDate res = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate();
-    return res;
+    Instant instant = Instant.ofEpochMilli(value.getTime());
+    return LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate();
   }
 
   public static LocalTime date2LocalTime(Date value) {
-    Date time = value;
-    Instant instant = Instant.ofEpochMilli(time.getTime());
-    LocalTime res = LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalTime();
-    return res;
+    Instant instant = Instant.ofEpochMilli(value.getTime());
+    return LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalTime();
+  }
+
+  public static Instant date2Instant(Date value) {
+    return Instant.ofEpochMilli(value.getTime());
   }
 
   public static Date localDateTime2Date(LocalDateTime value) {
-    LocalDateTime ldt = value;
-    Instant instant = ldt.atZone(ZoneId.systemDefault()).toInstant();
-    // FIXME Check if this is okay
-    Date res = new Date(instant.toEpochMilli());
-    return res;
+    Instant instant = value.atZone(ZoneId.systemDefault()).toInstant();
+    return new Date(instant.toEpochMilli());
+  }
+
+  public static String birthDateToStringFomattedUsingSlashesddMMYYYY(Date date) {
+    return birthDateToStringFomattedUsingPattern(date, "dd/MM/YYYY");
+  }
+
+  public static String birthDateToStringFomattedUsingDashesYYYYMMdd(Date date) {
+    return birthDateToStringFomattedUsingPattern(date, "YYYY-MM-dd");
+  }
+
+  public static String birthDateToStringFomattedUsingPattern(Date date, String pattern) {
+    Instant instant = Instant.ofEpochMilli(date.getTime());
+    DateTimeFormatter formatter =
+        DateTimeFormatter.ofPattern(pattern).withLocale(Locale.getDefault()).withZone(ZoneId.of("UTC").normalized());
+    return formatter.format(instant);
   }
 
   //public static Date localDate2Date(LocalDate value) {
