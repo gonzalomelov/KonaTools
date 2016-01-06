2dc0347a9d599583f3d8d306f3ab027d9514ed09
Gonzalo Melo <gonzalo@teamkona.io>
2016-01-06 11:34:45 -0300

    Remove hardcoded token

--- library/src/main/java/io/teamkona/konatools/network/AuthRequestInterceptor.java
+++ library/src/main/java/io/teamkona/konatools/network/AuthRequestInterceptor.java
@@ -23,14 +23,10 @@ public class AuthRequestInterceptor implements Interceptor {
   @Override public Response intercept(Chain chain) throws IOException {
     Request.Builder newRequestbuilder = chain.request().newBuilder();
 
-    //if (sessionManager.isSessionAlive()) {
-    //  newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
-    //      String.format("%s %s", HEADER_AUTHORIZATION_BEARER, sessionManager.getSession().getAuthToken()));
-    //}
-
-    // FIXME Remove token hardcoded
-    newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
-        String.format("%s %s", HEADER_AUTHORIZATION_BEARER, "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfX3YiOjAsIl9jcmVhdGVkQXQiOiIyMDE1LTEyLTIyVDIxOjIxOjE0LjQyNVoiLCJfdXBkYXRlZEF0IjoiMjAxNS0xMi0yMlQyMToyMToxNC40MjVaIiwiZW1haWwiOiJnb256YWxvbWVsb3ZAZ21haWwuY29tIiwiX2lkIjoiNTY3OWJlY2FlZjI0N2FiZDAzNzZjODE1IiwiZnJpZW5kcyI6W10sImZhdm91cml0ZVJlY29tbWVuZGVkIjpbXSwiZmF2b3VyaXRlRXZlbnRzIjpbXSwiaW50ZXJlc3RzIjpbXSwiY291bnRyaWVzVmlzaXRlZCI6W10sImNvdW50cmllc1RvVmlzaXQiOltdLCJjdXJyZW50UG9zaXRpb24iOltdLCJwcm9maWxlUGhvdG9zIjpbXX0.MZmG5xqXda_tgHZdHeU36GrAZbOAYlGm_HGTfbyfPe4"));
+    if (sessionManager.isSessionAlive()) {
+      newRequestbuilder.addHeader(HEADER_AUTHORIZATION,
+          String.format("%s %s", HEADER_AUTHORIZATION_BEARER, sessionManager.getSession().getAuthToken()));
+    }
 
     Request newRequest = newRequestbuilder.build();
 
